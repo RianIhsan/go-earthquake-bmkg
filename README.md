07e5924e6cf414c73fe9f81252f1ff838123c0cc
rianihsan28 <rianihsan28@outlook.com>
2024-04-30 13:15:43 +0700

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+## Fetch API BMKG dengan golang
+
+
+> 
+
+### GET ``/earthquake``
+Example Respon
+```
+{
+"Bujur": "140.61 BT",
+"Coordinates": "-2.57,140.61",
+"DateTime": "2024-04-29T19:43:35+00:00",
+"Dirasakan": "III Jayapura",
+"Jam": "02:43:35 WIB",
+"Kedalaman": "5 km",
+"Lintang": "2.57 LS",
+"Magnitude": "3.5",
+"Potensi": "Gempa ini dirasakan untuk diteruskan pada masyarakat",
+"Shakemap": "https://data.bmkg.go.id/DataMKG/TEWS/20240430024335.mmi.jpg",
+"Tanggal": "30 Apr 2024",
+"Wilayah": "Pusat gempa berada di darat 9 km Barat Daya Kota Jayapura"
+}
+```
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// Struktur data untuk informasi gempa dari BMKG
+type BMKGResponse struct {
+	Infogempa struct {
+		Gempa map[string]string `json:"gempa"`
+	} `json:"Infogempa"`
+}
+
+// Handler untuk endpoint gempa
+func earthquakeHandler(w http.ResponseWriter, r *http.Request) {
+	// Panggil API BMKG untuk mendapatkan informasi gempa terbaru
+	resp, err := http.Get("https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json")
+	if err != nil {
+		http.Error(w, "Failed to fetch earthquake data", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Dekode respons JSON
+	var bmkgResp BMKGResponse
+	if err := json.NewDecoder(resp.Body).Decode(&bmkgResp); err != nil {
+		http.Error(w, "Failed to decode earthquake data", http.StatusInternalServerError)
+		return
+	}
+
+	shakeMapURL := "https://data.bmkg.go.id/DataMKG/TEWS/" + bmkgResp.Infogempa.Gempa["Shakemap"]
+	bmkgResp.Infogempa.Gempa["Shakemap"] = shakeMapURL
+	// Kembalikan informasi gempa dalam format JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(bmkgResp.Infogempa.Gempa)
+}
+
+func main() {
+	// Routing endpoint
+	http.HandleFunc("/earthquake", earthquakeHandler)
+
+	// Mulai server di port 8080
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
